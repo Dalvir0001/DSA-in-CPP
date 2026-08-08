8bdf6ee8369cdb9394e32a6c754b898e9e84472e
Dalvir Singh <dalvir182600@gmail.com>
2026-08-08 19:17:18 +0530

    Add iterative inorder traversal

---
+++ Binary-Trees/Inorder_Iterative.cpp
@@ -0,0 +1,45 @@
+// Inorder Traversal of Binary Tree
+// Traversal: Left -> Root -> Right
+// Approach: Iterative using Stack
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st;
+
+        TreeNode* node = root;
+
+        while(node != nullptr || !st.empty())
+        {
+            while(node != nullptr)
+            {
+                st.push(node);
+                node = node->left;
+            }
+
+            node = st.top();
+            st.pop();
+
+            ans.push_back(node->val);
+            node = node->right;
+        }
+
+        return ans;
+    }
+};
