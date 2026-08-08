003e44d6ca77b026cefa8450e64d1dadf0f2056c
Dalvir Singh <dalvir182600@gmail.com>
2026-08-08 14:45:10 +0530

    Add recursive postorder traversal

---
+++ Binary-Trees/Postorder.cpp
@@ -0,0 +1,37 @@
+// Postorder Traversal of Binary Tree
+// Traversal: Left -> Right -> Root
+// Approach: Recursive
+// Time Complexity: O(n)
+// Space Complexity: O(h)
+
+// Code :-
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode *root, vector<int> &ans)
+    {
+        if(root == nullptr)
+        {
+            return;
+        }
+        solve(root->left, ans);
+        solve(root->right, ans);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        solve(root, ans);
+        return ans;
+    }
+};
