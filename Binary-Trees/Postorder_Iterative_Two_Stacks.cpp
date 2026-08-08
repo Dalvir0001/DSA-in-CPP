8af68c4938a11397ae3a67bc1fcba4ef346ad339
Dalvir Singh <dalvir182600@gmail.com>
2026-08-08 19:41:38 +0530

    Add iterative postorder traversal using two stacks

---
+++ Binary-Trees/Postorder_Iterative_Two_Stacks.cpp
@@ -0,0 +1,57 @@
+// Postorder Traversal of Binary Tree
+// Traversal: Left -> Right -> Root
+// Approach: Iterative using Two Stacks
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right)
+ *         : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        stack<TreeNode*> st1, st2;
+
+        if(root == nullptr)
+            return ans;
+
+        st1.push(root);
+
+        while(!st1.empty())
+        {
+            root = st1.top();
+            st1.pop();
+
+            st2.push(root);
+
+            if(root->left)
+            {
+                st1.push(root->left);
+            }
+
+            if(root->right)
+            {
+                st1.push(root->right);
+            }
+        }
+
+        while(!st2.empty())
+        {
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+
+        return ans;
+    }
+};
