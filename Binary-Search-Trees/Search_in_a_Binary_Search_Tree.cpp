7ce0e00bc921472a63334d8178f3f11f8a0313b0
Dalvir Singh <dalvir182600@gmail.com>
2026-08-21 18:08:15 +0530

    Add BST search solution

---
+++ Binary-Search-Trees/Search_in_a_Binary_Search_Tree.cpp
@@ -0,0 +1,27 @@
+// Search in a Binary Search Tree
+// Time Complexity: O(h)
+// Space Complexity: O(1)
+
+// Code :-
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root!=nullptr && root->val != val)
+        {
+            root = val < root->val ? root->left : root->right;
+        }
+        return root;
+    }
+};
